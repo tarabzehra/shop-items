43c6a1183609ee103f972929a1e473cf8147190b
tarab zehra <zehra4102818@cloud.neduet.edu.pk>
2021-04-06 20:52:32 +0500

    first

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ mongoose.connect(process.env.DB_CONNECTION,
     () => { console.log('connected to database') });
 
 http.createServer(app).listen(process.env.PORT || 3000, function () {
+    console.log(process.env.PORT);
 
     console.log('Example app listening on port', process.env.PORT || 3000)
 
